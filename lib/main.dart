19dcd80b8389468e95aae5d5b93f9b8f773c128b
Ardi Baskoro <mistersemar.ab@gmail.com>
2018-08-02 13:23:56 +0700

    add toolbar and text

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
+import './home.dart';
+
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   @override
-    Widget build(BuildContext context) {
-      // TODO: implement build
-      return MaterialApp(
-        home: Center(
-          child: Text("Hello, bentar lagi dimulai!"),
-        ),
-      );
-    }
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: HomePage(),
+    );
+  }
 }
